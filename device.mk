08ef91c770c1fa90c668d08a5d54f53a5a6216e9
Peter Cai <peter@typeblog.net>
2020-02-29 19:53:27 +0800

    allow switching notch style * Some may actually like the non-filled punchhole * 2 notch. * Removed toast message * Bring it back for OSS builds

--- device.mk
+++ device.mk
@@ -411,6 +411,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_phoenix/com.nxp.mifare.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
+# Notch style overlay
+PRODUCT_PACKAGES += \
+    NotchNoFillOverlay
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
