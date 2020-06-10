dc121ed0155e9e4b4368e46f571517a6f1f41a5d
GtrCraft <nickvanbokhorst@hotmail.com>
2020-06-10 11:22:43 +0200

    phoenix: Use stock v11.0.10.0 fingerprint
    Change-Id: I2c963513cf2a8cba67aea46288343cae58ccd51a

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -18,4 +18,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys
+BUILD_FINGERPRINT := POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.10.0.QGHINXM:user/release-keys
