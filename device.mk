25165cf756119f15d3c247722c4021a800b884ee
Akhil Narang <me@akhilnarang.dev>
2020-06-10 10:45:13 +0530

    phoenix: import keylayout
      * Disable DPAD_HOME
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- device.mk
+++ device.mk
@@ -91,6 +91,11 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.mi_thermald.rc
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_phoenix
