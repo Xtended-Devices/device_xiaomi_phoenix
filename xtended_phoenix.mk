ced0149caa58b6084e23863d74a95d8dd41289ab
GtrCraft <nickvanbokhorst@hotmail.com>
2020-06-14 20:55:43 +0200

    phoenix: Build pixelgapps

--- xtended_phoenix.mk
+++ xtended_phoenix.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/phoenix/device.mk)
 # Inherit some common Xtended stuff.
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
+# Inherit GApps
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
