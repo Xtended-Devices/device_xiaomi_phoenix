a8504c0da88e546bf64c5b86eb422f72bd5e0182
GtrCraft <nickvanbokhorst@hotmail.com>
2020-08-14 14:06:21 +0200

    phoenix: Update build fingerprints
    V12.0.3.0 for phoenix & V11.0.11.0 for phoenixin

--- init/init_phoenix.cpp
+++ init/init_phoenix.cpp
@@ -79,20 +79,20 @@ void vendor_load_properties() {
     if (region == "CN") {
         for (const auto &source : ro_props_default_source_order) {
             set_ro_build_prop(source, "fingerprint",
-                               "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+                               "Redmi/phoenix/phoenix:10/QKQ1.190825.002/V12.0.3.0.QGHCNXM:user/release-keys");
             set_ro_product_prop(source, "brand", "Redmi");
             set_ro_product_prop(source, "device", "phoenix");
             set_ro_product_prop(source, "model", "Redmi K30");
         }
-        property_override("ro.build.description", "phoenix-user 10 QKQ1.190825.002 V11.0.9.0.QGHCNXM release-keys");
-    } else {
+        property_override("ro.build.description", "phoenix-user 10 QKQ1.190825.002 V12.0.3.0.QGHCNXM release-keys");
+    } else if (region == "INDIA") {
         for (const auto &source : ro_props_default_source_order) {
             set_ro_build_prop(source, "fingerprint",
-                              "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+                              "POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.11.0.QGHINXM:user/release-keys");
             set_ro_product_prop(source, "brand", "POCO");
             set_ro_product_prop(source, "device", "phoenixin");
             set_ro_product_prop(source, "model", "POCO X2");
         }
-        property_override("ro.build.description", "phoenixin-user 10 QKQ1.190825.002 V11.0.6.0.QGHINXM release-keys");
+        property_override("ro.build.description", "phoenixin-user 10 QKQ1.190825.002 V11.0.11.0.QGHINXM release-keys");
     }
 }
