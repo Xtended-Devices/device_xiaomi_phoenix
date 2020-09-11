fb7b0d29474cf0762350000980b299a7a0136ba3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-12 01:02:48 +0530

    phoenix: add bootanim from pixel 4a
     * dark version, obviously
    
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     BluetoothQti
 
+# Bootanimation
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
