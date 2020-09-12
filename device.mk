94706436720bed515db8c2b6abd9195d8a22259d
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-09-12 18:51:11 +0530

    phoenix: Switch to aosp telephony
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -6,7 +6,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
