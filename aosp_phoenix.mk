1e314abe04d528d7384b5b891277e038a20367cb
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-09-17 19:01:14 +0530

    phoenix: We don't have vendor/aosp yet
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -6,9 +6,6 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_phoenix
 PRODUCT_DEVICE := phoenix
