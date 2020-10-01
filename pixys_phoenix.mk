a9e4333639445a770152edf228b7b33cf7e54678
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-10-01 20:25:17 +0530

    phoenix: PixysOS Bringup
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- pixys_phoenix.mk
+++ pixys_phoenix.mk
@@ -6,20 +6,19 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common PixysOS stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_phoenix
+PRODUCT_NAME := pixys_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
 
-# PixelExperience Properties
+# PixysOS Properties
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_AOSP_RECOVERY := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
