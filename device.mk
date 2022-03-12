be88296aa28535aa80ed580fa738083e7ff5bef1
ankan005 <ankan005@gmail.com>
2022-03-12 16:56:55 +0000

    parts related

--- device.mk
+++ device.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-parts.xml
 
+PRODUCT_PRODUCT_PROPERTIES += \
+     persist.disp.hbm_mode=0 \
+     persist.lcd.cabc_mode=1
+
 # Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
