5311ec995f7465c79975e4b657e0f19a86ee59bd
ankan005 <ankan005@gmail.com>
2022-03-16 07:13:41 +0000

    Revert "phoenix: parts: Adapt gaming profiles for our device"
    This reverts commit e39489d309c86f14fa6cd9d3678b80f54776b10e.

--- parts/src/org/lineageos/settings/thermal/ThermalService.java
+++ parts/src/org/lineageos/settings/thermal/ThermalService.java
@@ -80,6 +80,12 @@ public class ThermalService extends Service {
         return null;
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mThermalUtils.updateTouchRotation();
+    }
+
     private void registerReceiver() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_ON);
